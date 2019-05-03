9b76857b9d274f7f10b339c7cc456608f90e254b
Burr Sutter <burrsutter@gmail.com>
2019-05-03 13:59:42 -0400

    changes

--- 12_curl_greeter.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-
-SVC_URL=$(oc get ksvc greeter | awk 'NR==2{print $2}')
-
-curl $SVC_URL
--- 2_login.sh
+++ 2_login.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# oc login -u dev003 -p password https://api.guru.devx.red:6443
+oc login -u dev003 -p password https://api.guru.devx.red:6443
 
 # admin login
-oc login https://api.guru.devx.red:6443 -u kubeadmin -p password
+# oc login https://api.guru.devx.red:6443 -u kubeadmin -p password
 
