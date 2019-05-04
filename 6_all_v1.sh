de85e1d09ca3a10f23dfa5e640b86450ab88b0eb
Burr Sutter <burrsutter@gmail.com>
2019-05-04 11:51:07 -0400

    switching order of v2 to v1

--- 6_all_v1.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-oc create -f istio-tutorial/istiofiles/destination-rule-recommendation-v1-v2.yml 
-oc create -f istio-tutorial/istiofiles/virtual-service-recommendation-v1.yml 
--- 7_all_v2.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-oc replace -f istio-tutorial/istiofiles/virtual-service-recommendation-v2.yml 
